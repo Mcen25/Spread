5da0a86f9eac4116235c29d55f7cd3e5048f64fd
Matthew En <86386184+Mcen25@users.noreply.github.com>
2024-01-16 19:43:15 -0500

    Updated State machine

---
+++ Spread/Assets/Scripts/Minion State Machine/MinionStateMachine.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionStateMachine : BaseStateManager<MinionStateMachine.EMinionState>
+{
+    public enum EMinionState {
+        Idle,
+        Chase,
+        Attack,
+        Dead, 
+        Consume, 
+        Split, 
+        Combine,
+    }
+
+    // public Dictionary<EMinionState, MinionStateMachine> States { get; } = new Dictionary<EMinionState, MinionStateMachine>();
+
+    void Awake() {
+        // CurrentState = States[EMinionState.Idle];
+
+    }
+}

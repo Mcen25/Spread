53b24c738f3843452d59a8e47bc09b01d82d3ccd
Matthew En <86386184+Mcen25@users.noreply.github.com>
2024-01-16 19:22:44 -0500

    Removed old state machine

--- Spread/Assets/Scripts/Enemy AI/MinionBaseState.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public abstract class MinionBaseState : MonoBehaviour
-{
-    public abstract void EnterState(MinionStateManager minion);
-    public abstract void UpdateState(MinionStateManager minion);
-    public abstract void OnCollisionEnter(Collision collision);
-}

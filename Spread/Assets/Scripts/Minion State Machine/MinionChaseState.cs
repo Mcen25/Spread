8ee163a6ccd8bab6bb3ec5e85a5c5186b3fb589c
Matthew En <86386184+Mcen25@users.noreply.github.com>
2024-01-22 11:53:13 -0500

    Updated minion controller

--- Spread/Assets/Scripts/Minion State Machine/MinionChaseState.cs
+++ Spread/Assets/Scripts/Minion State Machine/MinionChaseState.cs
@@ -8,10 +8,7 @@ public class MinionChaseState : MinionBaseState
     public override void EnterState(MinionStateManager minion)
     {
         Debug.Log("Entering Chase State");
-        if (minion.animator == null)
-        {
-            Debug.Log("Animator is null");
-        }
+
         minion.audioSource.clip = minion.audioClips[0];
         minion.audioSource.Play();
         minion.animator.SetBool("isWalking", true);
@@ -30,11 +27,23 @@ public class MinionChaseState : MinionBaseState
     public override void OnCollisionEnter(MinionStateManager minion, Collision collision)
     {
         Debug.Log("Colliding in Chase State");
-        GameObject other = collision.gameObject;
-        if (other.CompareTag("Food"))
+    }
+
+    private GameObject FindClosestTarget(MinionStateManager minion) {
+        GameObject[] targets = GameObject.FindGameObjectsWithTag("Target");
+        GameObject closestTarget = null;
+        float closestDistance = Mathf.Infinity;
+
+        foreach (GameObject target in targets)
         {
-            minion.animator.SetBool("isWalking", false);
-            minion.SwitchState(minion.ConsumeState);
+            float distanceToTarget = Vector3.Distance(target.transform.position, minion.transform.position);
+            if (distanceToTarget < closestDistance)
+            {
+                closestDistance = distanceToTarget;
+                closestTarget = target;
+            }
         }
+
+        return closestTarget;
     }
 }
--- Spread/Assets/Scripts/Minion State Machine/MinionConsumeState.cs
+++ Spread/Assets/Scripts/Minion State Machine/MinionConsumeState.cs
@@ -6,23 +6,33 @@ public class MinionConsumeState : MinionBaseState
 {
 
     [SerializeField] private float distance = 100.0f;
+    
     public override void EnterState(MinionStateManager minion)
     {
         Debug.Log("Entering Consume State");
         minion.agent.SetDestination(minion.transform.position);
         minion.animator.SetBool("isWalking", false);
-        minion.animator.SetBool("IsEating", true);
-        GameObject food = minion.targets[0];
-        food.SetActive(false);
+        minion.animator.SetBool("isEating", true);
+
+        minion.StartCoroutine(DeactivateFoodAfterDelay(minion));
     }
 
     public override void UpdateState(MinionStateManager minion)
     {
-        
+        if (minion.targets[0].activeSelf == false) {
+            minion.SwitchState(minion.IdleState);
+        }
     }
 
     public override void OnCollisionEnter(MinionStateManager minion, Collision collision)
     {
         
+    }
+
+    private IEnumerator DeactivateFoodAfterDelay(MinionStateManager minion)
+    {
+        Debug.Log("Waiting to deactivate food");
+        yield return new WaitForSeconds(15f);
+        minion.targets[0].SetActive(false);
     }
 }
